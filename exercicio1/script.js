fe05d81d442559806a1a1a31d777bd7e7fe31e30
helenrgs <lenlenregiss@gmail.com>
2022-11-27 10:12:51 -0300

    condicionais II.helen

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,19 @@
-console.log("teste 1")
+// # Exercício 1
 
+// Crie um código que receba um número por **prompt** e verifique se um número é divisível por 2 **ou** por 3
+const parOuNao =() => {
+    numero = Number(prompt("Digite um número"))
+    if (numero%2 == 0 || numero % 3 == 0) {
+        console.log("O número é divisível por 2 ou 3")
+    }
+}
+    
+// 2. Utilizando um operador lógico para unir duas operações relacionais
+// ```jsx
+//     if(expressao && expressao){ //&& para E
+//     //expressao || expressao para OU     
+    
+//     }
+// ```
+
+parOuNao()
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,17 +1,46 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+// Vamos recriar o exercício Nacionalidade da aula de condicionais I, utilizando switch-case. 
+
+// -> Relembrando o enunciado.
+
+// Crie uma função que receba do usuário sua nacionalidade, utilize um prompt que diga: 
+const diversaoNacionalidade = (nacionalidade) => {
+    nacionalidade = (prompt("Qual a sua nacionalidade?")).toLowerCase()
+
+    switch (nacionalidade){
+        case 'brasileira':
+            console.log(`Você é ${nacionalidade}`)
+            break;
+        case 'argentina':
+            console.log(`Você é ${nacionalidade}`)
+            break
+        case 'uruguaia':
+            console.log(`Você é ${nacionalidade}`)
+            break
+        case 'chilena':
+            console.log(`Você é ${nacionalidade}`)
+            break
+        case 'colombiana':
+            console.log(`Você é ${nacionalidade}`)
+            break
+        default:
+            console.log('nacionalidade não encontrada')
+    }
 }
+//"Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const nacionalidade.
 
-console.log("teste 2")
+// Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+
+// Crie uma estrutura com o switch-case que verifique se a nacionalidade guardada na const nacionalidade, 
+//é igual a cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. 
+//O programa deve imprimir "nacionalidade não encontrada" 
+//caso o valor de nacionalidade não corresponda a nenhum dos valores.
+
+
+diversaoNacionalidade()
